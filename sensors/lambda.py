a134a3c1dcff3ea9db1bb6a74da49bfa24cfab28
Jim Haughwout <jim.haughwout@gmail.com>
2017-08-18 07:56:42 -0400

    Overnight test results

--- sensors/lambda.py
+++ sensors/lambda.py
@@ -6,6 +6,7 @@ from time import sleep
 
 def get_location(scan_payload):
     API_KEY = 'AIzaSyDcv-DsSncE_0J70_bf8DAXBfT887fsWXs'
+    RESULT_AVAILABLE = 'GeolocationUpdate'
     NO_RESULT_AVAILABLE = 'NoLocationAvailable'
 
     url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=%s' % API_KEY
@@ -13,11 +14,11 @@ def get_location(scan_payload):
     r = requests.post(url, json=payload)
     if r.status_code == 200:
     	results = json.loads(r.text)
-    	scan_payload['event'] = 'GeolocationUpdate'
+    	scan_payload['event'] = RESULT_AVAILABLE
     	scan_payload['location'] = results['location']
     	scan_payload['accuracy'] = results['accuracy']
     else:
-        scan_payload['event'] = 'NoLocationAvailable'
+        scan_payload['event'] = NO_RESULT_AVAILABLE
         scan_payload['location'] = None
         scan_payload['accuracy'] = None
     return scan_payload
@@ -30,13 +31,13 @@ def log_results(log_file, location_results):
 
 LOG_FILE = 'HAUGHWOUT_MAC_BELLFOREST'
 INTERVAL = 900 # Seconds
-READ_CNT = 12
+READ_CNT = 40
 
-for i in range(0, READ_CNT):
-	print "Scan %3d..." % (i + 1),
+for i in range(1, READ_CNT + 1):
+	print "Scan: %s..." % str(i).zfill(4),
 	scan = mac_os_location_scan()
 	print "Getting location for scan results...",
 	location = get_location(scan)
 	log_results(LOG_FILE, location)
-	print "Logged results"
+	print "Logged results. Waiting %d seconds" % INTERVAL
 	sleep(INTERVAL)
